9f18d92c6c96e95d52eb5e9753ff1100b584eb1e
Tommy Ivarsson <tommy.ivarsson@cdon.com>
2021-05-07 11:02:30 +0200

    Add Namespace test

--- CdonMarketplace.Tests/Contracts/Product/NamespaceTests.cs
+++ CdonMarketplace.Tests/Contracts/Product/NamespaceTests.cs
@@ -5,10 +5,10 @@ using Xunit;
 
 namespace CdonMarketplace.Tests.Contracts.Product
 {
-    public class ProductTests
+    public class NamespaceTests
     {
         [Fact]
-        public void ShouldSerializeProduct()
+        public void ShouldNotUseNamespacesOnElements()
         {
             var marketplace = new Marketplace
             {
@@ -19,14 +19,15 @@ namespace CdonMarketplace.Tests.Contracts.Product
                         Identity = new Identity
                         {
                             Id = "Test"
-                        },
-                        Usp = new List<UspValue>(),
+                        }
                     }
                 }
             };
 
             var xml = XmlUtils.SerializeXml(marketplace);
-            const string expected = "<?xml version=\"1.0\" encoding=\"utf-8\"?><marketplace xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns=\"https://schemas.cdon.com/product/4.0/4.9.0/product\"><product><identity><id>Test</id></identity></product></marketplace>";
+            const string expected =
+                "<?xml version=\"1.0\" encoding=\"utf-8\"?><marketplace xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns=\"https://schemas.cdon.com/product/4.0/4.9.0/product\"><product><identity><id>Test</id></identity></product></marketplace>";
+
             Assert.Equal(expected, xml);
         }
     }
